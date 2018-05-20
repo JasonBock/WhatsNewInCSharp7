199101a11721ba564669f3618c830a8cd9212eeb
Jason Bock <jason@jasonbock.net>
2018-05-20 11:23:00 -0500

    Updated projects to reflect correct C# version for features demonstrated.

--- WhatsNewInCSharp7/LotsOfProperties.cs
+++ WhatsNewInCSharp7/LotsOfProperties.cs
@@ -5,7 +5,7 @@ namespace WhatsNewInCSharp7
 	public sealed class LotsOfProperties
 	{
 		public LotsOfProperties(Guid id, string name, uint age) =>
-		  (this.Id, this.Name, this.Age) = (id, name, age);
+			(this.Id, this.Name, this.Age) = (id, name, age);
 
 		public Guid Id { get; }
 		public string Name { get; }
